2c637d13d7fc83c6989a6c18bea4a3d363fb71a7
osamakhanedu <osamakhanedu@gmail.com>
2026-05-26 13:16:45 +0400

    convert to event instead of readline

---
+++ 1brc-step1.js
@@ -0,0 +1,103 @@
+import * as readline from "node:readline/promises";
+import * as fs from "node:fs";
+
+const filename = process.argv[2];
+const stream = fs.createReadStream(filename);
+// const linestream = readline.createInterface(stream);
+
+const NEW_LINE_CHARACTER = '\n'.charCodeAt(0);
+const SEMICOLON_CHARACTER = ';'.charCodeAt(0);
+const LOOKING_FOR_SEMICOLON = 0;
+const LOOKING_FOR_NEWLINE = 1;
+
+
+let state = LOOKING_FOR_SEMICOLON;
+let stationBuffer = Buffer.alloc(100);  // 
+let tempBuffer = Buffer.alloc(5);
+let stationBufferIndex = 0;
+let tempBufferIndex = 0;
+
+const aggregations = new Map();
+
+stream.on('end', () => {
+    stream.close();
+    printCompiledResults(aggregations);
+})
+
+stream.on('data', (chunk) => {
+
+    for (let i = 0; i < chunk.length; i++) {
+        const byte = chunk[i];
+
+
+        if (state === LOOKING_FOR_SEMICOLON) {
+
+            if (byte === SEMICOLON_CHARACTER) {
+                state = LOOKING_FOR_NEWLINE
+            } else {
+                stationBuffer[stationBufferIndex] = byte;
+                stationBufferIndex++;
+            }
+
+        } else if (state === LOOKING_FOR_NEWLINE) {
+            if (byte === LOOKING_FOR_NEWLINE) {
+                const temp = Number(tempBuffer.toString('utf-8', 0, tempBufferIndex)) * 10;
+                const stationName = stationBuffer.toString('utf-8', 0, stationBufferIndex);
+
+                const existing = aggregations.get(stationName);
+
+                if (!existing) {
+                    aggregations.set(stationName, {
+                        min: temp,
+                        max: temp,
+                        sum: temp,
+                        count: 1
+                    });
+                } else {
+                    existing.min = Math.min(existing.min, temp);
+                    existing.max = Math.max(existing.max, temp);
+                    existing.sum += temp;
+                    existing.count += 1;
+                }
+
+                state = LOOKING_FOR_SEMICOLON;
+
+                stationBufferIndex = 0;
+                tempBufferIndex = 0;
+
+            } else {
+                tempBuffer[tempBufferIndex] = byte;
+                tempBufferIndex += 1;
+            }
+        }
+
+    }
+})
+
+
+
+printCompiledResults(aggregations);
+
+function printCompiledResults(aggregations) {
+    const sortedStations = Array.from(aggregations.keys()).sort();
+
+    let result =
+        '{' +
+        sortedStations
+            .map((station) => {
+                const data = aggregations.get(station);
+                return `${station}=${round(data.min / 10)}/${round(
+                    data.sum / 10 / data.count
+                )}/${round(data.max / 10)}`;
+            })
+            .join(', ') +
+        '}';
+
+    console.log(result);
+}
+
+function round(num) {
+    const fixed = Math.round(10 * num) / 10;
+
+    return fixed.toFixed(1);
+}
